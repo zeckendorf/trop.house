1b6332bae9d7a2d044eb54ca9f140c6a60621623
Sam Zeckendorf <Sam.Zeckendorf@spacex.com>
2018-10-24 01:31:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##🌴🌴 Tropical Vibes On Demand 🌴🌴
+
+Take a trip to some troppy vibes through some crappy, low-res textures using borrowed math and hastily thrown-together API calls. 
+
+Spotify users only kthx, everyone else can leave px audi
+
+[Special thanks to @jakesgordon](https://github.com/jakesgordon/javascript-racer) for math and stuff
+
---
+++ tropical.js
@@ -0,0 +1,89 @@
+//Get hash of the url
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = '858e4ac240f0435b8859c168cb2e2711';
+const redirectUri = 'http://localhost:8000/tropical.html'
+const scopes = [
+  'streaming',
+  'user-read-birthdate',
+  'user-read-private',
+  'user-modify-playback-state'
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+}
+
+// Set up the Web Playback SDK
+window.onSpotifyPlayerAPIReady = () => {
+  const player = new Spotify.Player({
+    name: 'Web Playback SDK Template',
+    getOAuthToken: cb => { cb(_token); }
+  });
+
+  // Error handling
+  player.on('initialization_error', e => console.error(e));
+  player.on('authentication_error', e => console.error(e));
+  player.on('account_error', e => console.error(e));
+  player.on('playback_error', e => console.error(e));
+
+  // Playback status updates
+  player.on('player_state_changed', state => {
+    console.log(state)
+    $('#current-track').attr('src', state.track_window.current_track.album.images[0].url);
+    $('#current-track-name').text(state.track_window.current_track.name);
+  });
+
+  // Ready
+  player.on('ready', data => {
+    console.log('Ready with Device ID', data.device_id);
+    
+    // Play a track using our new device ID
+    shuffle(data.device_id);
+  });
+
+  // Connect to the player!
+  player.connect();
+}
+
+function shuffle(device_id) {
+   // set to shuffle
+   $.ajax({
+    url: "https://api.spotify.com/v1/me/player/shuffle?state=true",
+    type: "PUT",
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+   success: play(device_id) //switch to complete to shuffle from V beginning
+  });
+}
+
+// Play a specified track on the Web Playback SDK's device ID
+function play(device_id) {
+   
+  // begin playlist
+  $.ajax({
+   url: "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
+   type: "PUT",
+   data: '{"context_uri": "spotify:user:szeck:playlist:6WGQxH3t845oiMNfwkUWux"}',
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+   success: function(data) { 
+     console.log(data)
+   }
+  });
+}
